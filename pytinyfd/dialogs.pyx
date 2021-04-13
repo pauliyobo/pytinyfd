7964d2f5609b9479379486fb1ffed3f0d5e52e87
pauliyobo <pauliyobo@gmail.com>
2021-04-13 12:33:15 +0200

    reppresenting icon, button, and dialog options with enums

--- pytinyfd/dialogs.pyx
+++ pytinyfd/dialogs.pyx
@@ -1,10 +1,32 @@
+from enum import Enum, IntEnum
+
 from dialogs cimport *
 
-def  notify_popup(title, message, icon_type="info"):
-    return tinyfd_notifyPopupW(title, message, icon_type)
+class Icon(Enum):
+    ERROR =    "error"
+    INFO = "info"
+    QUESTION = "question"
+    WARNING = "warning"
 
-def message_box(title, message, dialog_type="ok", icon_type="info", default_button=1):
-    return tinyfd_messageBoxW(title, message, dialog_type, icon_type, default_button)
+class DialogType(Enum):
+    OK = "ok"
+    CANCEL = "cancel"
+    YESNO = "yesno"
+
+class Button(IntEnum):
+    CANCEL = 0
+    OK = 1
+
+class Input(Enum):
+    PASSWORD  = None
+    TEXT = ""
+
+
+def  notify_popup(title, message, icon_type=Icon.INFO):
+    return tinyfd_notifyPopupW(title, message, icon_type.value)
+
+def message_box(title, message, dialog_type=DialogTypes.OK, icon_type=Icon.INFO, default_button=Button.OK):
+    return tinyfd_messageBoxW(title, message, dialog_type.value, icon_type.value, default_button.value)
 
 def save_dialog(title, default_path_and_file, num_of_filter_patterns=0, filter_patterns="", filter_description=""):
     return tinyfd_saveFileDialogW(title, default_path_and_file, num_of_filter_patterns, NULL, filter_description)
