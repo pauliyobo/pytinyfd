a903966dc20fc13ee8efd220986f31cab636dcd7
pauliyobo <pauliyobo@gmail.com>
2021-04-17 13:42:36 +0200

    replaced tabs with 4 spaces for indentation

--- setup.py
+++ setup.py
@@ -4,18 +4,19 @@ from Cython.Distutils import build_ext
  
 
 macros = [
-	("_WIN32", None),
+    ("_WIN32", None),
 ]
 
 extensions = [Extension(
-	"pytinyfd.dialogs",
-	["pytinyfd/dialogs.pyx", "lib/tinyfiledialogs.c"],
-	define_macros=macros,
-	libraries = "Shell32 User32 Comdlg32 Ole32".split(" "),
+    "pytinyfd.dialogs",
+    ["pytinyfd/dialogs.pyx", "lib/tinyfiledialogs.c"],
+    define_macros=macros,
+    language_level=3,
+    libraries = "Shell32 User32 Comdlg32 Ole32".split(" "),
 )]
 
 setup(
-	name = "pytinyfd",
-	cmdclass  = {"build_ext": build_ext},
-	ext_modules = cythonize(extensions),
+    name = "pytinyfd",
+    cmdclass  = {"build_ext": build_ext},
+    ext_modules = cythonize(extensions),
 )
